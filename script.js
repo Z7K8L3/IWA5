a71b02450e8b9f5f488089f778d4e82561b5b7ec
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-08-10 09:42:39 +0200

    Changed location to userLocation

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// constants for messages
+// Constants for messages
 const FREE_WARNING = 'Free shipping only applies to single customer orders';
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
 const NONE_SELECTED = 0;
 
-// item costs
+// Item costs
 const shoes = 300 * 1;
 const toys = 100 * 5
 const shirts = 150 * NONE_SELECTED;
 const batteries = 35 * 2;
 const pens = 5 * NONE_SELECTED;
 
-const location = 'RSA'; //User location (RSA, NAM, NK)
+const userLocation = 'RSA'; //User userLocation (RSA, NAM, NK)
 const currency = 'R'; //User currency (R, $)
 const customers = 1; //Number of customers
 
@@ -19,25 +19,31 @@ let totalCost = 0;
 
 totalCost = shoes + toys + shirts + batteries + pens;
 
-// shipping cost based on location
-if (location === 'RSA') { 
-    shipping = 400 && currency === 'R';
-} else if (location === 'NAM') {
+// Shipping cost based on userLocation
+if (userLocation === 'RSA') {
+    if (currency === 'R') {
+        shipping = 400 ;
+    }
+} else if (userLocation === 'NAM') {
     shipping = 600;
 } else {
     shipping = 800;
 }
 
-if (location === 'NK') {
+// If user is from NK and if free shipping applies
+if (userLocation === 'NK') {
     console.log(BANNED_WARNING);
+} else {
+    if (totalCost >= 1000 && (userLocation === 'RSA' || userLocation === 'NAM') && customers === 1) {
+        shipping = 0;
 }
 
-if (totalCost >= 1000 && (location === 'RSA' || location === 'NAM') && customers < 2) {
-		    shipping;
-}
-
-if (shipping = 0 && customers !== 1) {
+// When warning message should be displayed
+if (shipping === 0 && customers !== 1) {
     console.log(FREE_WARNING);
 }
 
-console.log('price:', currency, totalCost + shipping);
+// Display the total price
+const totalPrice = totalCost + shipping;
+console.log('Price:', currency, totalPrice);
+}
